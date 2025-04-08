f1883bdcde127f0c3015fe00f653444442593937
Wen Zhen <wlzeign@hotmail.com>
2025-04-08 16:55:43 +0800

    update icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,13 @@ export default function Document() {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
+
+
+        {/* Favicon */}
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+        <link rel="shortcut icon" href="/icon.svg" type="image/svg+xml" />
+
       </Head>
       <body>
         <Main />
